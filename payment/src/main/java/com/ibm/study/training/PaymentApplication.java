32729cd81f2172579983ee3a21cb2a11faf03270
Ethan <157259312@qq.com>
2019-11-17 19:08:35 +0800

    update

--- payment/src/main/java/com/ibm/study/training/PaymentApplication.java
+++ payment/src/main/java/com/ibm/study/training/PaymentApplication.java
@@ -2,14 +2,22 @@ package com.ibm.study.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.transaction.jta.JtaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(exclude = {JtaAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @ComponentScan("com.ibm.study")
+@EnableJpaRepositories(basePackages = "com.ibm.study.training.dao")
+@EntityScan(basePackages = "com.ibm.study.training.entity")
+@EnableTransactionManagement
 @EnableDiscoveryClient
+@EnableFeignClients
 public class PaymentApplication {
 
     public static void main(String[] args) {
